67a1deb997385d95e195ddd8ee8394374f6b5155
Pavan Mudigonda <mnpawan@gmail.com>
2021-11-16 20:34:00 -0500

    Update action.ps1

--- action.ps1
+++ action.ps1
@@ -66,7 +66,7 @@ function Publish-ToCheckRun {
 
     Write-ActionInfo "Publishing Report to GH Workflow"
 
-    $ghToken = '${{secrets_PS_TOKEN}}'
+    $ghToken = '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'
     $ctx = Get-ActionContext
     $repo = Get-ActionRepo
     $repoFullName = "$($repo.Owner)/$($repo.Repo)"
